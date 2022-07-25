deb8854625293b52785c91e5e27f2fa81b5507c6
viktor-vvd <68917448+viktor-vvd@users.noreply.github.com>
2022-07-25 21:05:24 +0300

    Upload

---
+++ index.php
@@ -0,0 +1,382 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: rpz32
+ * Date: 03.03.2020
+ * Time: 16:15
+ */
+
+echo "<script>
+        var count=1;
+        function A11f()
+        {
+            if(count%2!=0)
+            {
+                if(document.getElementById(\"ia11\").style.display==\"none\")
+                {
+                    document.getElementById(\"ia11\").src=\"https://pngimage.net/wp-content/uploads/2018/06/letra-x-png-2.png\";
+                    document.getElementById(\"ia11\").style.display=\"block\";
+                    count++;
+                }
+
+            }
+            else if(count%2==0)
+            {
+                if(document.getElementById(\"ia11\").style.display==\"none\")
+                {
+                    document.getElementById(\"ia11\").src=\"https://pngimage.net/wp-content/uploads/2018/06/o-png-2.png\";
+                    document.getElementById(\"ia11\").style.display=\"block\";
+                    count++;
+                }
+
+            }
+            Prov()
+        }
+        function A12f()
+        {
+            if(count%2!=0)
+            {
+                if(document.getElementById(\"ia12\").style.display==\"none\")
+                {
+                    document.getElementById(\"ia12\").src=\"https://pngimage.net/wp-content/uploads/2018/06/letra-x-png-2.png\";
+                    document.getElementById(\"ia12\").style.display=\"block\";
+                    count++;
+                }
+
+            }
+            else if(count%2==0)
+            {
+                if(document.getElementById(\"ia12\").style.display==\"none\")
+                {
+                    document.getElementById(\"ia12\").src=\"https://pngimage.net/wp-content/uploads/2018/06/o-png-2.png\";
+                    document.getElementById(\"ia12\").style.display=\"block\";
+                    count++;
+                }
+
+            }
+            Prov()
+        }
+        function A13f()
+        {
+            if(count%2!=0)
+            {
+                if(document.getElementById(\"ia13\").style.display==\"none\")
+                {
+                    document.getElementById(\"ia13\").src=\"https://pngimage.net/wp-content/uploads/2018/06/letra-x-png-2.png\";
+                    document.getElementById(\"ia13\").style.display=\"block\";
+                    count++;
+                }
+
+            }
+            else if(count%2==0)
+            {
+                if(document.getElementById(\"ia13\").style.display==\"none\")
+                {
+                    document.getElementById(\"ia13\").src=\"https://pngimage.net/wp-content/uploads/2018/06/o-png-2.png\";
+                    document.getElementById(\"ia13\").style.display=\"block\";
+                    count++;
+                }
+
+            }
+            Prov()
+        }
+        //
+        function A21f()
+        {
+            if(count%2!=0)
+            {
+                if(document.getElementById(\"ia21\").style.display==\"none\")
+                {
+                    document.getElementById(\"ia21\").src=\"https://pngimage.net/wp-content/uploads/2018/06/letra-x-png-2.png\";
+                    document.getElementById(\"ia21\").style.display=\"block\";
+                    count++;
+                }
+
+            }
+            else if(count%2==0)
+            {
+                if(document.getElementById(\"ia21\").style.display==\"none\")
+                {
+                    document.getElementById(\"ia21\").src=\"https://pngimage.net/wp-content/uploads/2018/06/o-png-2.png\";
+                    document.getElementById(\"ia21\").style.display=\"block\";
+                    count++;
+                }
+
+            }
+            Prov()
+        }
+        function A22f()
+        {
+            if(count%2!=0)
+            {
+                if(document.getElementById(\"ia22\").style.display==\"none\")
+                {
+                    document.getElementById(\"ia22\").src=\"https://pngimage.net/wp-content/uploads/2018/06/letra-x-png-2.png\";
+                    document.getElementById(\"ia22\").style.display=\"block\";
+                    count++;
+                }
+
+            }
+            else if(count%2==0)
+            {
+                if(document.getElementById(\"ia22\").style.display==\"none\")
+                {
+                    document.getElementById(\"ia22\").src=\"https://pngimage.net/wp-content/uploads/2018/06/o-png-2.png\";
+                    document.getElementById(\"ia22\").style.display=\"block\";
+                    count++;
+                }
+
+            }
+            Prov()
+        }
+        function A23f()
+        {
+            if(count%2!=0)
+            {
+                if(document.getElementById(\"ia23\").style.display==\"none\")
+                {
+                    document.getElementById(\"ia23\").src=\"https://pngimage.net/wp-content/uploads/2018/06/letra-x-png-2.png\";
+                    document.getElementById(\"ia23\").style.display=\"block\";
+                    count++;
+                }
+
+            }
+            else if(count%2==0)
+            {
+                if(document.getElementById(\"ia23\").style.display==\"none\")
+                {
+                    document.getElementById(\"ia23\").src=\"https://pngimage.net/wp-content/uploads/2018/06/o-png-2.png\";
+                    document.getElementById(\"ia23\").style.display=\"block\";
+                    count++;
+                }
+
+            }
+            Prov()
+        }
+        //
+        function A31f()
+        {
+            if(count%2!=0)
+            {
+                if(document.getElementById(\"ia31\").style.display==\"none\")
+                {
+                    document.getElementById(\"ia31\").src=\"https://pngimage.net/wp-content/uploads/2018/06/letra-x-png-2.png\";
+                    document.getElementById(\"ia31\").style.display=\"block\";
+                    count++;
+                }
+
+            }
+            else if(count%2==0)
+            {
+                if(document.getElementById(\"ia31\").style.display==\"none\")
+                {
+                    document.getElementById(\"ia31\").src=\"https://pngimage.net/wp-content/uploads/2018/06/o-png-2.png\";
+                    document.getElementById(\"ia31\").style.display=\"block\";
+                    count++;
+                }
+
+            }
+            Prov()
+        }
+        function A32f()
+        {
+            if(count%2!=0)
+            {
+                if(document.getElementById(\"ia32\").style.display==\"none\")
+                {
+                    document.getElementById(\"ia32\").src=\"https://pngimage.net/wp-content/uploads/2018/06/letra-x-png-2.png\";
+                    document.getElementById(\"ia32\").style.display=\"block\";
+                    count++;
+                }
+
+            }
+            else if(count%2==0)
+            {
+                if(document.getElementById(\"ia32\").style.display==\"none\")
+                {
+                    document.getElementById(\"ia32\").src=\"https://pngimage.net/wp-content/uploads/2018/06/o-png-2.png\";
+                    document.getElementById(\"ia32\").style.display=\"block\";
+                    count++;
+                }
+
+            }
+            Prov()
+        }
+        function A33f()
+        {
+            if(count%2!=0)
+            {
+                if(document.getElementById(\"ia33\").style.display==\"none\")
+                {
+                    document.getElementById(\"ia33\").src=\"https://pngimage.net/wp-content/uploads/2018/06/letra-x-png-2.png\";
+                    document.getElementById(\"ia33\").style.display=\"block\";
+                    count++;
+                }
+
+            }
+            else if(count%2==0)
+            {
+                if(document.getElementById(\"ia33\").style.display==\"none\")
+                {
+                    document.getElementById(\"ia33\").src=\"https://pngimage.net/wp-content/uploads/2018/06/o-png-2.png\";
+                    document.getElementById(\"ia33\").style.display=\"block\";
+                    count++;
+                }
+
+            }
+            Prov()
+        }
+        function Prov(){
+            if(count>=5)
+            {
+                if(document.getElementById(\"ia11\").style.display!=\"none\"&&
+                    document.getElementById(\"ia11\").src==document.getElementById(\"ia13\").src&&
+                    document.getElementById(\"ia12\").src==document.getElementById(\"ia13\").src)
+                {
+                    if(count%2==0)
+                    {
+                        alert(\"X win!\");
+                    }
+                    else if(count%2!=0)
+                    {
+                        alert(\"O win!\");
+                    }
+                }
+                if(document.getElementById(\"ia21\").style.display!=\"none\"&&
+                    document.getElementById(\"ia21\").src==document.getElementById(\"ia23\").src&&
+                    document.getElementById(\"ia22\").src==document.getElementById(\"ia23\").src)
+                {
+                    if(count%2==0)
+                    {
+                        alert(\"X win!\");
+                    }
+                    else if(count%2!=0)
+                    {
+                        alert(\"O win!\");
+                    }
+                }
+                if(document.getElementById(\"ia31\").style.display!=\"none\"&&
+                    document.getElementById(\"ia31\").src==document.getElementById(\"ia33\").src&&
+                    document.getElementById(\"ia32\").src==document.getElementById(\"ia33\").src)
+                {
+                    if(count%2==0)
+                    {
+                        alert(\"X win!\");
+                    }
+                    else if(count%2!=0)
+                    {
+                        alert(\"O win!\");
+                    }
+                }
+                //
+                if(document.getElementById(\"ia11\").style.display!=\"none\"&&
+                    document.getElementById(\"ia11\").src==document.getElementById(\"ia31\").src&&
+                    document.getElementById(\"ia21\").src==document.getElementById(\"ia31\").src)
+                {
+                    if(count%2==0)
+                    {
+                        alert(\"X win!\");
+                    }
+                    else if(count%2!=0)
+                    {
+                        alert(\"O win!\");
+                    }
+                }
+                if(document.getElementById(\"ia12\").style.display!=\"none\"&&
+                    document.getElementById(\"ia12\").src==document.getElementById(\"ia32\").src&&
+                    document.getElementById(\"ia22\").src==document.getElementById(\"ia32\").src)
+                {
+                    if(count%2==0)
+                    {
+                        alert(\"X win!\");
+                    }
+                    else if(count%2!=0)
+                    {
+                        alert(\"O win!\");
+                    }
+                }
+                if(document.getElementById(\"ia13\").style.display!=\"none\"&&
+                    document.getElementById(\"ia13\").src==document.getElementById(\"ia33\").src&&
+                    document.getElementById(\"ia23\").src==document.getElementById(\"ia33\").src)
+                {
+                    if(count%2==0)
+                    {
+                        alert(\"X win!\");
+                    }
+                    else if(count%2!=0)
+                    {
+                        alert(\"O win!\");
+                    }
+                }
+                //
+                if(document.getElementById(\"ia11\").style.display!=\"none\"&&
+                    document.getElementById(\"ia22\").src==document.getElementById(\"ia33\").src&&
+                    document.getElementById(\"ia11\").src==document.getElementById(\"ia33\").src)
+                {
+                    if(count%2==0)
+                    {
+                        alert(\"X win!\");
+                    }
+                    else if(count%2!=0)
+                    {
+                        alert(\"O win!\");
+                    }
+                }
+                if(document.getElementById(\"ia13\").style.display!=\"none\"&&
+                    document.getElementById(\"ia22\").src==document.getElementById(\"ia31\").src&&
+                    document.getElementById(\"ia13\").src==document.getElementById(\"ia31\").src)
+                {
+                    if(count%2==0)
+                    {
+                        alert(\"X win!\");
+                    }
+                    else if(count%2!=0)
+                    {
+                        alert(\"O win!\");
+                    }
+                }
+            }
+        }
+    </script>
+<div id=\"Arr\">
+    <div id=\"a11\" style=\"width: 50px; height: 50px; background-color: aquamarine; display: inline-block\" onclick=\"A11f()\">
+        <img src=\"\" style=\"width: 50px; height: 50px;display: none\" id=\"ia11\">
+    </div>
+    <div id=\"a12\" style=\"width: 50px; height: 50px; background-color: aquamarine; display: inline-block\"onclick=\"A12f()\">
+        <img src=\"\" style=\"width: 50px; height: 50px;display: none\" id=\"ia12\">
+    </div>
+    <div id=\"a13\" style=\"width: 50px; height: 50px; background-color: aquamarine; display: inline-block\"onclick=\"A13f()\">
+        <img src=\"\" style=\"width: 50px; height: 50px; display: none\" id=\"ia13\">
+    </div>
+    <br>
+    <div id=\"a21\" style=\"width: 50px; height: 50px; background-color: aquamarine; display: inline-block\"onclick=\"A21f()\">
+        <img src=\"\" style=\"width: 50px; height: 50px;display: none\" id=\"ia21\">
+    </div>
+    <div id=\"a22\" style=\"width: 50px; height: 50px; background-color: aquamarine; display: inline-block\"onclick=\"A22f()\">
+        <img src=\"\" style=\"width: 50px; height: 50px;display: none\" id=\"ia22\">
+    </div>
+    <div id=\"a23\" style=\"width: 50px; height: 50px; background-color: aquamarine; display: inline-block\"onclick=\"A23f()\">
+        <img src=\"\" style=\"width: 50px; height: 50px;display: none\" id=\"ia23\">
+    </div>
+    <br>
+    <div id=\"a31\" style=\"width: 50px; height: 50px; background-color: aquamarine; display: inline-block\"onclick=\"A31f()\">
+        <img src=\"\" style=\"width: 50px; height: 50px;display: none\" id=\"ia31\">
+    </div>
+    <div id=\"a32\" style=\"width: 50px; height: 50px; background-color: aquamarine; display: inline-block\"onclick=\"A32f()\">
+        <img src=\"\" style=\"width: 50px; height: 50px;display: none\" id=\"ia32\">
+    </div>
+    <div id=\"a33\" style=\"width: 50px; height: 50px; background-color: aquamarine; display: inline-block\"onclick=\"A33f()\">
+        <img src=\"\" style=\"width: 50px; height: 50px;display: none\" id=\"ia33\">
+    </div>
+
+</div>";
+?>
+<html>
+<head>
+</head>
+<body>
+<form action="dz.php">
+    <input type="submit">
+</form>
+</body>
+</html>
